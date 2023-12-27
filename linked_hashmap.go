995603879460e7bc54ca288e16f35a7137406bb1
Ethan Water <watermonic@gmail.com>
2023-12-27 15:31:22 -0500

    consistent hashing impl

--- linked_hashmap.go
+++ linked_hashmap.go
@@ -162,6 +162,40 @@ func (lhm *LinkedHashMap[K, V]) distributeMap(servers []Server[K, V]) error{
 	return nil
 }
 
+//consistent hashing algortihm using a 'ring', preferred over 'distrbuteMap'
+const RING uint64 = 360 //default RING size
+
+func (lhm *LinkedHashMap[K, V]) distrbuteConsistentHash(servers []Server[K, V], ring uint64) error {
+	if len(servers) == 0 {
+		return errors.New("no servers available for distribution")
+	}
+	serverDistance := ring / uint64(len(servers))
+
+	var determineServer func(uint64, uint64, uint64) int
+	determineServer = func(hash , ring, dist uint64) int {
+		initialServer := 1
+		
+		for i:=int(dist) ; i<int(ring); i+=int(dist) {
+			if hash < uint64(i) {
+				return initialServer
+			}
+			initialServer++
+		}
+
+		return 0
+	}
+
+	for _, bucket := range lhm.buckets {
+		for _, kv := range bucket {
+      hash := Hasher(bytify(kv.key)) % ring //[0, 360)
+			focusedServer := servers[determineServer(hash, serverDistance, ring)]
+			focusedServer.table.Insert(kv.key, kv.value)
+			continue
+		}
+	}
+	return nil
+}
+
 //doesn't cover all types*
 func bytify[T any] (data T) []byte {
 	value := reflect.ValueOf(data)
